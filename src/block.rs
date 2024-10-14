21acb20a56c45db2432aedfc2b0aae43a69df88e
Skythrill256 <anjishnuganguly773@gmail.com>
2024-10-14 14:30:53 +0530

    first commit

--- src/block.rs
+++ src/block.rs
@@ -2,16 +2,20 @@
 pub struct Block {
     pub index: usize,
     pub hash: String,
-    pub previous_hash: String,
+    pub previous_hash: Option<String>,
     pub data: String,
     pub timestamp: u64,
+}
+
+pub struct Blockchain {
+    chain: Vec<Block>,
 }
 
 impl Block {
     pub fn new(
         index: usize,
         hash: String,
-        previous_hash: String,
+        previous_hash: Option<String>,
         data: String,
         timestamp: u64,
     ) -> Self {
@@ -22,5 +26,13 @@ impl Block {
             data,
             timestamp,
         }
+    }
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        let mut chain = Vec::new();
+        chain.push(Self::create_genesis_block());
+        Blockchain { chain }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod block;
 mod hash;
 fn main() {
-    println!("Hello, world!");
+    let genesis_block = block::Block::new(
+        0,
+        String::from("816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"),
+        None,
+        String::from("Genesis Block"),
+        1728895903,
+    );
 }
